d9319f276127bd7fe9d78798393a7ac5c0ce1cb5
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-02-03 12:33:08 +0100

    Loggers

--- src/main/java/pl/training/batch/BatchConfiguration.java
+++ src/main/java/pl/training/batch/BatchConfiguration.java
@@ -1,5 +1,7 @@
 package pl.training.batch;
 
+import lombok.extern.java.Log;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepExecutionListener;
@@ -9,15 +11,32 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.job.DefaultJobParametersValidator;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.listener.ExecutionContextPromotionListener;
+import org.springframework.batch.core.step.tasklet.CallableTaskletAdapter;
+import org.springframework.batch.core.step.tasklet.MethodInvokingTaskletAdapter;
+import org.springframework.batch.core.step.tasklet.SystemCommandTasklet;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.CompletionPolicy;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.repeat.policy.CompositeCompletionPolicy;
+import org.springframework.batch.repeat.policy.SimpleCompletionPolicy;
+import org.springframework.batch.repeat.policy.TimeoutTerminationPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
+@Log
 @EnableBatchProcessing
 @Configuration
 public class BatchConfiguration {
@@ -49,6 +68,77 @@ public class BatchConfiguration {
     }
 
     @Bean
+    public MethodInvokingTaskletAdapter methodInvokingTaskletAdapter(AccountService accountService) {
+        var adapter = new MethodInvokingTaskletAdapter();
+        adapter.setTargetObject(accountService);
+        adapter.setTargetMethod("incrementBalance");
+        adapter.setArguments(new Object[] { 20L });
+        return adapter;
+    }
+
+    @Bean
+    public CallableTaskletAdapter callableTaskletAdapter() {
+        var adapter = new CallableTaskletAdapter();
+        adapter.setCallable(() -> {
+            log.info("Executing background task...");
+            return RepeatStatus.FINISHED;
+        });
+        return adapter;
+    }
+
+    @Bean
+    public SystemCommandTasklet systemCommandTasklet() {
+        var adapter = new SystemCommandTasklet();
+        adapter.setCommand("ls");
+        adapter.setTimeout(2_000);
+        adapter.setWorkingDirectory("/Users/lukas");
+        adapter.setEnvironmentParams(new String[] { "JAVA_HOME=/java" });
+        adapter.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        return adapter;
+    }
+
+    @Bean
+    public Step secondStep(MethodInvokingTaskletAdapter methodInvokingTaskletAdapter) {
+        return stepBuilderFactory.get("secondStep")
+                .tasklet(methodInvokingTaskletAdapter)
+                .build();
+    }
+
+    @Bean
+    public ItemReader<String> itemReader() {
+        var items = new ArrayList<String>(200);
+        for (int i = 0; i < 200; i++) {
+            items.add(UUID.randomUUID().toString());
+        }
+        return new ListItemReader<>(items);
+    }
+
+    @Bean
+    public ItemWriter<String> itemWriter() {
+        return items -> {
+            log.info("### New chunk");
+            items.forEach(System.out::println);
+        };
+    }
+
+    @Bean
+    public CompletionPolicy completionPolicy() {
+        var policy = new CompositeCompletionPolicy();
+        policy.setPolicies(new CompletionPolicy[] { new TimeoutTerminationPolicy(1_000), new SimpleCompletionPolicy(100)});
+        return policy;
+    }
+
+    @Bean
+    public Step thirdStep() {
+        return stepBuilderFactory.get("thirdStep")
+                //.<String, String>chunk(100)
+                .<String, String>chunk(completionPolicy())
+                .reader(itemReader())
+                .writer(itemWriter())
+                .build();
+    }
+
+    @Bean
     public DefaultJobParametersValidator defaultJobParametersValidator() {
         var validator = new DefaultJobParametersValidator();
         validator.setRequiredKeys(new String[] {"fileName"});
@@ -64,14 +154,38 @@ public class BatchConfiguration {
     }
 
     @Bean
-    public Job job(Step firstStep) {
-        return jobBuilderFactory.get("job5")
+    public Flow processingFlow(Step firstStep, Step secondStep, Step thirdStep) {
+        return new FlowBuilder<Flow>("processingFlow")
+                .start(firstStep)
+                .next(secondStep)
+                .next(thirdStep)
+                .build();
+    }
+
+    @Bean
+    public Step fourthStep(Flow processingFlow) {
+        return stepBuilderFactory.get("fourthStep")
+                //.job(custom_custom)
+                .flow(processingFlow)
+                .build();
+    }
+
+    @Bean
+    public Job firstJob(Step fourthStep, Flow processingFlow) {
+        return jobBuilderFactory.get("firstJob2")
                 .validator(validators())
                 //.incrementer(new RunIdIncrementer())
                 .incrementer(new ExecutionDateIncrementer())
                 .listener(new JobExecutionLogger())
-                .start(firstStep)
+                .start(fourthStep)
+                //.next(new CustomDecider())
+                //.on(ExitStatus.FAILED.getExitDescription()).to(processingFlow)
+                //.next(someStep)
                 .build();
+
+                //.start(fourthStep)
+                //.build();
     }
+
 
 }
